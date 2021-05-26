b5bfa5e8925ea316bc0896a294b1641201e5cf64
AleksiusK <kurkela.aleksius@gmail.com>
2021-05-26 15:02:57 +0300

    Modified the app.js file

--- contracts/SolidityDrive.sol
+++ contracts/SolidityDrive.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.1;
+pragma solidity 0.5.16;
 
 
 contract SolidityDrive {
@@ -10,16 +10,17 @@ contract SolidityDrive {
     }
 
     mapping(address => File[]) public files;
-    funtion add(string memory _hash, string memory _filename, string memory _filetype, uint _date) public {
+    
+    function add(string memory _hash, string memory _filename, string memory _filetype, uint _date) public {
            files[msg.sender].push(File({hash: _hash, filename: _filename, filetype: _filetype, date: _date}));
        }
 
     function getFile(uint _index) public view returns(string memory, string memory, string memory, uint) {
-        File memory file = files[msg.sende][_index];
+        File memory file = files[msg.sender][_index];
         return (file.hash, file.filename, file.filetype, file.date);
     }
 
     function getLenght() public view returns(uint) {
-        return files[msg.sender].lenght;
+        return files[msg.sender].length;
     }
 }
